480bc428a9ddbd1284c43b39d6fd2e73bc27b4fa
akshay07-max <akshaysurase992@gmail.com>
2026-09-10 09:14:33 +0530

    setup

--- model.py
+++ model.py
@@ -1,7 +1,13 @@
 from openai import OpenAI
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+api_key=os.getenv("API_KEY")
 client = OpenAI()
 
 response = client.response.create(model="gpt-6-astra", input="Write a short bedtime story about a unicorn.")
 
 print(response.output_text) 
+
+
